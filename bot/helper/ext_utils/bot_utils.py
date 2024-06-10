a26be47b415e63ab35abd00bc52cbfae8aa88953
ivachloe <ivachloe@petalmail.com>
2024-06-10 15:57:28 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -199,7 +199,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = "<b><a href='https://t.me/JetMirror'>Pᴏᴡᴇʀᴇᴅ ʙʏ ᴊᴇᴛ-ᴍɪʀʀᴏʀ 🚀♥️</a></b>\n\n"
+    msg = "<b><a href='https://t.me/lovelyhacker_143'>Pᴏᴡᴇʀᴇᴅ ʙʏ lovelyhacker_143 💞</a></b>\n\n"
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
